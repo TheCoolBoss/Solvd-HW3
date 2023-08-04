c2ec9ae9d9cf1e4f3aae3a7762b6c6f472a739e0
TheCoolBoss <stedn2014@gmail.com>
2023-08-04 15:01:20 -0700

    Data provider for logins now used

--- hw3/src/test/java/hw3/carina/demo/hw/web/LoginTest.java
+++ hw3/src/test/java/hw3/carina/demo/hw/web/LoginTest.java
@@ -3,37 +3,48 @@ package hw3.carina.demo.hw.web;
 import com.zebrunner.carina.utils.R;
 import hw3.carina.demo.gui.pages.hw.web.HomePage;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import com.zebrunner.carina.core.IAbstractTest;
 import hw3.carina.demo.gui.pages.hw.web.LoginPage;
 
 public class LoginTest implements IAbstractTest
 {
-    @Test()
-    public void testFailedLogin()
+    @Test(dataProvider = "credentials")
+    public void testLogin(boolean isGood, String user, String pass)
     {
-        String user = R.TESTDATA.get("bad_user");
-        String pass = R.TESTDATA.get("bad_pass");
-
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.open();
         Assert.assertTrue(loginPage.isPageOpened(), "Not on login page");
 
-        loginPage.sendLoginInfo(user, pass);
-        Assert.assertTrue(loginPage.isPageOpened(), "Not on login page");
+        if (!isGood)
+        {
+            loginPage.sendLoginInfo(user, pass);
+            Assert.assertTrue(loginPage.isPageOpened(), "Login page should be current page due to failed credentials");
+        }
+
+        else
+        {
+            HomePage homePage = loginPage.loginToHome(user, pass);
+            Assert.assertTrue(homePage.isPageOpened(), "Not at home page");
+        }
     }
 
-    @Test()
-    public void testGoodLogin()
+    @DataProvider(name = "credentials")
+    public static Object[][] credentialProvider()
     {
-        String user = R.TESTDATA.get("good_user");
-        String pass = R.TESTDATA.get("good_pass");
-
-        LoginPage loginPage = new LoginPage(getDriver());
-        loginPage.open();
-        Assert.assertTrue(loginPage.isPageOpened(), "Not on login page");
-
-        HomePage homePage = loginPage.loginToHome(user, pass);
-        Assert.assertTrue(homePage.isPageOpened(), "Not at home page");
+        return new Object[][]
+                {
+                        {
+                                false,
+                                R.TESTDATA.get("bad_user"),
+                                R.TESTDATA.get("bad_pass")
+                        },
+                        {
+                                true,
+                                R.TESTDATA.get("good_user"),
+                                R.TESTDATA.get("good_pass")
+                        }
+                };
     }
 }
